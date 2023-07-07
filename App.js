bc39d1f2a1916b6b1365dc085ea2660e6c68f7e0
Leonardo Palavecino <64588981+leo43161@users.noreply.github.com>
2023-07-07 15:44:53 -0300

    Se creo el modelo base para seguir con el proyecto

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import Home from './src/Screens/Home';
+import { colors } from './src/Global/Colors';
+import Header from './src/Components/Header';
 
 export default function App() {
+  const [categorySelected, setCategorySelected] = useState("")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header></Header>
+      <Home/>
     </View>
   );
 }
@@ -13,8 +17,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.lightOcean
   },
 });
---
+++ src/Global/Colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    darkBlue: "#001C30",
+    blue: "#176B87",
+    ocean: "#64CCC5",
+    lightOcean: "#DAFFFB"
+}
